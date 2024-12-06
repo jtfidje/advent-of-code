f4997c3efdf4103e51a25557bf82305ad88f29ed
Jahn Thomas Fidje <jtfidje@gmail.com>
2024-12-06 07:03:05 +0100

    Stash

---
+++ src/2024/python/day-06/solver/part_1.py
@@ -0,0 +1,55 @@
+# flake8: noqa: F401
+
+from pathlib import Path
+from typing import Literal
+
+from solver import utils
+
+data_path = Path(__file__).parent.parent.absolute() / "data"
+
+
+def solve(path: str | Path):
+    data = utils.read_lines(path)
+    data = [list(line) for line in data]
+
+    obstacle_positions = set()
+    for row_i, row in enumerate(data):
+        for col_i, col in enumerate(row):
+            match col:
+                case "^":
+                    guard_pos = (row_i, col_i)
+                case "#":
+                    obstacle_positions.add((row_i, col_i))
+
+    state: Literal["^", "v", ">", "<"] = "^"
+    next_state: dict[str, Literal["^", "v", ">", "<"]] = {
+        "^": ">",
+        ">": "v",
+        "v": "<",
+        "<": "^",
+    }
+    move_map = {"^": (-1, 0), "v": (1, 0), ">": (0, 1), "<": (0, -1)}
+
+    unique = set([guard_pos])
+    while True:
+        new_pos = (
+            guard_pos[0] + move_map[state][0],
+            guard_pos[1] + move_map[state][1],
+        )
+
+        if new_pos in obstacle_positions:
+            state = next_state[state]
+            continue
+
+        if utils.out_of_bounds(*new_pos, data):
+            break
+
+        guard_pos = new_pos
+        unique.add(guard_pos)
+
+    return len(unique)
+
+
+if __name__ == "__main__":
+    answer = solve(Path(data_path, "input.txt"))
+    print(f"Problem 1: {answer}")

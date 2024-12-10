4de3351e6d338633c20ef63506890daf884fc3d6
Jahn Thomas Fidje <jtfidje@gmail.com>
2024-12-10 06:35:03 +0100

    solve

---
+++ src/2024/python/day-10/solver/part_2.py
@@ -0,0 +1,48 @@
+# flake8: noqa: F401
+
+from pathlib import Path
+
+from solver import utils
+
+data_path = Path(__file__).parent.parent.absolute() / "data"
+
+
+def solve(path: str | Path):
+    data = utils.read_lines(path)
+    # data = [list(map(int, line)) for line in data]
+    data = [list(line) for line in data]
+
+    starting_points = [
+        (row_i, col_i)
+        for row_i, row in enumerate(data)
+        for col_i, col in enumerate(row)
+        if col == "0"
+    ]
+
+    total_score = 0
+    for starting_point in starting_points:
+        stack = [starting_point]
+        score = 0
+        # tops = set()
+        while stack:
+            pos = stack.pop(-1)
+            row_i, col_i = pos
+            value = data[row_i][col_i]
+
+            if value == "9":
+                score += 1
+                continue
+
+            adjacent = utils.get_adjacent(row_i, col_i, data, include_corners=False)
+            for point in adjacent:
+                if data[point[0]][point[1]] == str(int(value) + 1):
+                    stack.append(point)
+
+        total_score += score
+
+    return total_score
+
+
+if __name__ == "__main__":
+    answer = solve(Path(data_path, "input.txt"))
+    print(f"Problem 2: {answer}")

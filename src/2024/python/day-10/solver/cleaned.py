33adb2a44119e1e216424c58776dc94efb31868e
Jahn Thomas Fidje <jtfidje@gmail.com>
2024-12-10 06:54:22 +0100

    Add cleaned solution

--- src/2024/python/day-10/solver/cleaned.py
+++ src/2024/python/day-10/solver/cleaned.py
@@ -1,16 +1,74 @@
 from pathlib import Path
 
-from solver.utils import read_lines
+from solver import utils
 
 data_path = Path(__file__).parent.parent.absolute() / "data"
 
 
+def get_starting_positions(topo_map: list[list[int]]) -> list[tuple[int, int]]:
+    return [
+        (row_i, col_i)
+        for row_i, row in enumerate(topo_map)
+        for col_i, col in enumerate(row)
+        if col == 0
+    ]
+
+
 def solve_1(path: Path):
-    data = read_lines(path)
+    topo_map = [list(map(int, line)) for line in utils.read_lines(path)]
+    starting_points = get_starting_positions(topo_map)
+
+    trailhead_score = 0
+    for starting_point in starting_points:
+        stack = [starting_point]
+        visited = set()
+        while stack:
+            pos = stack.pop(-1)
+
+            if pos in visited:
+                continue
+            visited.add(pos)
+
+            row_i, col_i = pos
+            value = topo_map[row_i][col_i]
+
+            if value == 9:
+                trailhead_score += 1
+                continue
+
+            for point in utils.get_adjacent(
+                row_i, col_i, topo_map, include_corners=False
+            ):
+                if topo_map[point[0]][point[1]] == value + 1:
+                    stack.append(point)
+
+    return trailhead_score
 
 
 def solve_2(path: Path):
-    data = read_lines(path)
+    topo_map = [list(map(int, line)) for line in utils.read_lines(path)]
+    starting_points = get_starting_positions(topo_map)
+
+    trailhead_score = 0
+    for starting_point in starting_points:
+        stack = [starting_point]
+        while stack:
+            pos = stack.pop(-1)
+
+            row_i, col_i = pos
+            value = topo_map[row_i][col_i]
+
+            if value == 9:
+                trailhead_score += 1
+                continue
+
+            for point in utils.get_adjacent(
+                row_i, col_i, topo_map, include_corners=False
+            ):
+                if topo_map[point[0]][point[1]] == value + 1:
+                    stack.append(point)
+
+    return trailhead_score
 
 
 if __name__ == "__main__":

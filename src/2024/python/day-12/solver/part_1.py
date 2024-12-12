5c043787ce328c24cf28fc68d4edeb6dbcc16b41
Jahn Thomas Fidje <jtfidje@gmail.com>
2024-12-12 18:31:13 +0100

    stash

--- src/2024/python/day-12/solver/part_1.py
+++ src/2024/python/day-12/solver/part_1.py
@@ -1,6 +1,12 @@
 # flake8: noqa: F401
-
+import math
+from functools import cache
 from pathlib import Path
+
+import matplotlib.path as pltPath
+import shapely.geometry
+import shapely.geometry.polygon
+import sympy
 
 from solver import utils
 
@@ -54,13 +60,61 @@ def solve(path: str | Path):
     region_map = {tuple(region): 0 for region in regions}
 
     # Calculate perimiters
-    for region, value in region_map:
+    for region in region_map:
         # We need to check if a region is _inside_ another region
-        ...
 
-    return 0
+        min_row = min_col = math.inf
+        max_row = max_col = -math.inf
+        for row_i, col_i in region:
+            min_row = min(min_row, row_i)
+            min_col = min(min_col, col_i)
+            max_row = max(max_row, row_i)
+            max_col = max(max_col, col_i)
+
+        region_map[region] += _calculate_permiter(region)
+
+        for _region in region_map:
+            if _region == region:
+                continue
+
+            if len(_region) < 8:
+                continue
+
+            polygon = _create_polygon(_region)
+            breakpoint()
+            res = [polygon.encloses_point(sympy.Point(*point)) for point in region]
+            print(_region)
+            print(region)
+            print(res)
+            print()
+            if all(res):
+                region_map[_region] += _calculate_permiter(region)
+
+    for region, perimiter in region_map.items():
+        print(len(region), perimiter)
+
+    return sum(len(region) * perimiter for region, perimiter in region_map.items())
+
+
+@cache
+def _create_polygon(points):
+    return sympy.Polygon(*points)
+
+
+@cache
+def _calculate_permiter(points):
+    min_row = min_col = math.inf
+    max_row = max_col = -math.inf
+    for row_i, col_i in points:
+        min_row = min(min_row, row_i)
+        min_col = min(min_col, col_i)
+        max_row = max(max_row, row_i)
+        max_col = max(max_col, col_i)
+
+    return int(((max_col - min_col + 1) * 2) + ((max_row - min_row + 1) * 2))
 
 
 if __name__ == "__main__":
-    answer = solve(Path(data_path, "input.txt"))
+    # answer = solve(Path(data_path, "input.txt"))
+    answer = solve(Path(data_path, "example_1_3.txt"))
     print(f"Problem 1: {answer}")

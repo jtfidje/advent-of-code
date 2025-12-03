ec86b4cc75d3c203f41a7ebadb39720db18c592f
Jahn Thomas Fidje <jtfidje@gmail.com>
2025-12-03 08:03:37 +0100

    chore: Format all tables in README, and update template

--- puzzles/aoc-2024-01/src/aoc_2024_01/__init__.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-DATA_PATH = Path(__file__).parent.parent.parent.resolve() / "data"
-
-__all__ = ["DATA_PATH"]
--- puzzles/aoc-2024-01/src/aoc_2024_01/utils.py
+++
@@ -1,1 +0,0 @@
-from advent_of_code.utils import *  # noqa: F403

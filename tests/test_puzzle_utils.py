947f84535b2b2b56328d3a3c2920aac334dc1932
Jahn Thomas Fidje <jtfidje@gmail.com>
2025-12-06 08:08:54 +0100

    test: Add more utils tests

--- tests/test_puzzle_utils.py
+++ tests/test_puzzle_utils.py
@@ -1,4 +1,5 @@
 import json
+from typing import Any
 
 import pytest
 
@@ -24,10 +25,51 @@ def test_read_lines(tmp_path):
     assert utils.read_lines(test_file) == ["Line 1", "Line 2", "Line 3"]
 
 
-def test_read_numbers(tmp_path):
+@pytest.mark.parametrize(
+    ["input_string", "expected"],
+    [["Hello 3.14 world -3.14!\nWith 1337 integer", [3.14, -3.14, 1337.0]]],
+)
+def test_parse_floats(input_string: str, expected: list[float]):
+    assert utils.parse_floats(input_string) == expected
+
+
+def test_parse_floats__as_generator():
+    input_string = "Hello 3.14 World!"
+
+    gen = utils.parse_floats(input_string, as_generator=True)
+
+    assert list(gen) == [3.14]
+    assert list(gen) == []
+
+
+@pytest.mark.parametrize(
+    ["input_string", "expected"],
+    [["Hello -13 world 37!\nWith 3.14 float", [-13, 37, 3, 14]]],
+)
+def test_parse_integers(input_string: str, expected: list[int]):
+    assert utils.parse_integers(input_string) == expected
+
+
+def test_parse_integers__as_generator():
+    input_string = "Hello 1337 World!"
+
+    gen = utils.parse_integers(input_string, as_generator=True)
+
+    assert list(gen) == [1337]
+    assert list(gen) == []
+
+
+@pytest.mark.parametrize(
+    ["input_string", "expected"],
+    [
+        ["1\n2\n3", [1, 2, 3]],
+        ["Multiple 13 37 numbers\non 3.14 lines", [13, 37, 3, 14]],
+    ],
+)
+def test_read_numbers(input_string: str, expected: list[int], tmp_path):
     test_file = tmp_path / "test.txt"
-    test_file.write_text("1\n2\n3")
-    assert utils.read_numbers(str(test_file)) == [1, 2, 3]
+    test_file.write_text(input_string)
+    assert utils.read_numbers(str(test_file)) == expected
 
 
 @pytest.mark.parametrize(
@@ -112,8 +154,22 @@ def test_out_of_bounds(inp: tuple[int, int], expected: bool):
             {"width": 2, "height": 2},
             {(0, 0), (0, 1), (0, 2), (1, 0), (2, 0)},
         ),
+        (
+            (1, 1),
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            {"return_values": True},
+            {1, 2, 3, 4, 6, 7, 8, 9},
+        ),
     ],
 )
-def test_get_adjacent(pos, matrix, kwargs, expected):
+def test_get_adjacent(
+    pos: tuple[int, int],
+    matrix: list[list[int]],
+    kwargs: dict[str, Any],
+    expected: set,
+):
     x, y = pos
-    assert utils.get_adjacent(x, y, matrix, **kwargs) == expected
+    res = utils.get_adjacent(x, y, matrix, **kwargs)
+
+    assert len(res) == len(expected)
+    assert set(res) == expected

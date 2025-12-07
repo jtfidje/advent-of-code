0f3f6b35bdf75d320bc20d015c8657da58b8d7cb
Jahn Thomas Fidje <jtfidje@gmail.com>
2025-12-07 21:14:15 +0100

    chore: Update template for cleaned.py to use the new performance_timer decorator

--- src/aoc_template/templates/cleaned.py
+++ src/aoc_template/templates/cleaned.py
@@ -5,10 +5,12 @@ from __PKG__ import DATA_PATH
 from advent_of_code import utils
 
 
+@utils.performance_timer
 def solve_1(path: Path):
     data = utils.read_lines(path)
 
 
+@utils.performance_timer
 def solve_2(path: Path):
     data = utils.read_lines(path)
 
@@ -16,6 +18,6 @@ def solve_2(path: Path):
 if __name__ == "__main__":
     answer = solve_1(DATA_PATH / "input.txt")
     print(f"Problem 1: {answer}")
-
+    print()
     answer = solve_2(DATA_PATH / "input.txt")
     print(f"Problem 2: {answer}")

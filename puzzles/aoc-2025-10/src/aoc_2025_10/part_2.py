ca9f587b71d594227a16aa192bd9ee065eb43557
Jahn Thomas Fidje <jtfidje@gmail.com>
2025-12-10 20:10:25 +0100

    stash non-numpy

--- puzzles/aoc-2025-10/src/aoc_2025_10/part_2.py
+++ puzzles/aoc-2025-10/src/aoc_2025_10/part_2.py
@@ -5,20 +5,20 @@ import re
 from pathlib import Path
 from typing import Self
 
-import numpy as np
-
 from advent_of_code import utils
 from aoc_2025_10 import DATA_PATH
 
 
 class Node:
-    def __init__(self, press_id: int, state: np.ndarray, parent: None | Self = None):
+    def __init__(
+        self, press_id: int, state: dict[int, int], parent: None | Self = None
+    ):
         self.press_id = press_id
         self.state = state
-        self.state_tuple: tuple[np.int64, ...] = tuple(state)
+        self.state_tuple = tuple(self.state[i] for i in range(len(self.state)))
         self.parent = parent
 
-    def get_state_path(self) -> list[tuple[int, tuple[np.int64, ...]]]:
+    def get_state_path(self) -> list[tuple[int, tuple[int, ...]]]:
         node = self
         states = [(node.press_id, node.state_tuple)]
         while node.parent:
@@ -33,49 +33,35 @@ def solve(path: str | Path):
 
     result = 0
     for line in data:
-        _, buttons_data, joltage = re.findall(
-            r"\[([\.#]+)\] (\(.*\)) (\{[\d,]+\})", line
-        )[0]
+        _, buttons, joltage = re.findall(r"\[([\.#]+)\] (\(.*\)) (\{[\d,]+\})", line)[0]
 
-        target = np.array(utils.parse_integers(joltage))
-        buttons = []
-        for idx in sorted(
-            list(map(utils.parse_integers, buttons_data.split(" "))),
+        target = {i: x for i, x in enumerate(utils.parse_integers(joltage))}
+        buttons = sorted(
+            list(map(utils.parse_integers, buttons.split(" "))),
             reverse=False,
             key=lambda x: len(x),
-        ):
-            arr = np.zeros(shape=target.shape, dtype=np.int64)
-            for i in idx:
-                arr[i] += 1
-
-            buttons.append(arr)
+        )
 
         best = math.inf
-        visited: dict[tuple[np.int64, ...], int] = {}
-        to_visit = [
-            Node(press_id=0, state=np.zeros(shape=target.shape, dtype=np.int64))
-        ]
-
+        visited: dict[tuple[int, ...], int] = {}
+        to_visit = [Node(press_id=0, state={key: 0 for key in target})]
         while to_visit:
             node = to_visit.pop(-1)
 
-            if all(node.state == target):
-                if node.press_id < best:
-                    best = node.press_id
-                    to_visit = [n for n in to_visit if n.press_id < best]
-                    visited = {
-                        state: press_id for press_id, state in node.get_state_path()
-                    }
-
+            if node.state == target:
+                best = min(node.press_id, best)
+                to_visit = [n for n in to_visit if n.press_id < best]
+                visited = {state: press_id for press_id, state in node.get_state_path()}
                 continue
 
             new_nodes: list[Node] = []
             for button in buttons:
-                new_state = node.state.copy()
-                new_state += button
-
-                if any(new_state == target):
+                if any(node.state[i] == target[i] for i in button):
                     continue
+
+                new_state = {key: value for key, value in node.state.items()}
+                for i in button:
+                    new_state[i] += 1
 
                 new_node = Node(press_id=node.press_id + 1, state=new_state)
 
@@ -90,7 +76,10 @@ def solve(path: str | Path):
 
                 new_nodes.append(new_node)
 
-            new_nodes.sort(key=lambda n: sum(target - n.state), reverse=True)
+            new_nodes.sort(
+                key=lambda n: sum(target[i] - n.state[i] for i in target.keys()),
+                reverse=True,
+            )
             to_visit.extend(new_nodes)
         result += best
 
